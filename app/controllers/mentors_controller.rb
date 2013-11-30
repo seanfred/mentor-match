d005d0f439ed22c9380405d4c61506fef2c58e52
Michael Frankenfield <mfrank183@gmail.com>
2013-11-30 15:26:27 -0800

    set student mentor controllers back to template

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,152 +1,82 @@
 class MentorsController < ApplicationController
-  before_filter :authorize_admins_or_students_or_a_mentor!,
-                  only:   [ :show]
-  before_filter :authorize_admins_or_a_mentor!,
-                  only:   [ :edit,
-                            :update,
-                            :destroy]
+
   before_filter :authenticate_user!
   before_filter :current_user_approved?,
                   except: [ :new,
                             :create]
 
   def index
-    if current_user.mentor
-      @user = user
-      @studentsinterest = Student.joins(:user).where("approval = 1 AND goals_companies=?", @user.career_company_private).order(:id).page(params[:page]).per(20)
-      @students = Student.joins(:user).where("approval = 1").order(:id).page(params[:page]).per(20)
-      @paired_students = user.students.order(:personal_first_name).page(params[:page]).per(20)
-      @current_mentees = Student.joins(:user).where("approval = 1 AND mentor_id = ?", @user.id).order(:id).page(params[:page]).per(20)
-      @rankings = current_user.mentor.rankings.order(:rank)
-      @classes = [["Amazon", "Interested in #{user.career_company_private.capitalize}", "company"], ["rails2", "From June Class"], ["rails3", "From July Class"]]
-      if params[:company]
-        @students = Student.joins(:user).where("approval = 1 AND goals_companies = ?", params[:sort]).order(:id)
-      elsif params[:sort] == 'all'
-        @students = Student.joins(:user).where("approval= ?", 1).order(:id)
-      elsif params[:sort]
-        @students = Student.joins(:user).where("approval = 1 AND cf_class = ?", params[:sort]).order(:id)
-      end
-    else
-      flash[:alert] = "You don't have access to that page."
-      redirect_to root_path
-    end
+    @mentors = Mentor.all
+
     respond_to do |format|
-      format.html
-      format.js
+      format.html # index.html.erb
+      format.json { render json: @mentors }
     end
   end
 
-  def new
-    if current_user.role == "mentor" && !current_user.mentor
-      @mentor = Mentor.new
-    else
-      redirect_to mentors_path
-    end
-  end
 
   def show
+    @mentor = Mentor.find(params[:id])
+
     respond_to do |format|
-      format.html
-      format.js
+      format.html # show.html.erb
+      format.json { render json: @mentor }
     end
   end
+
+
+  def new
+    @mentor = Mentor.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @mentor }
+    end
+  end
+
 
   def edit
-    respond_to do |format|
-      format.html
-      format.js
-    end
+    @mentor = Mentor.find(params[:id])
   end
+
 
   def create
-    if current_user.role == "mentor" && !current_user.mentor
-      @mentor = Mentor.new(params[:mentor])
-      @mentor.user_id = current_user.id
+    @mentor = Mentor.new(params[:mentor])
 
+    respond_to do |format|
       if @mentor.save
-        redirect_to '/thanks'
+        format.html { redirect_to @mentor, notice: 'Mentor was successfully created.' }
+        format.json { render json: @mentor, status: :created, location: @mentor }
       else
-        flash[:alert] = 'Sorry, there was a problem. ' +
-                        'Please make sure your first name, last name, job title, company, company type ' +
-                        '& email are all filled in.'
-        render :action => "new"
+        format.html { render action: "new" }
+        format.json { render json: @mentor.errors, status: :unprocessable_entity }
       end
-    else
-      mentors_path
     end
   end
+
 
   def update
     @mentor = Mentor.find(params[:id])
 
-    if user == @mentor || current_user.admin
-      update_mentor(@mentor)
-    else
-      flash[:notice] = "You can only edit your own profile."
-      redirect_to mentors_path
+    respond_to do |format|
+      if @mentor.update_attributes(params[:mentor])
+        format.html { redirect_to @mentor, notice: 'Mentor was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @mentor.errors, status: :unprocessable_entity }
+      end
     end
   end
+
 
   def destroy
     @mentor = Mentor.find(params[:id])
-    firstname = @mentor.personal_first_name
-    lastname = @mentor.personal_last_name
-    if current_user.mentor == @mentor
-      current_user.destroy
-      redirect_to root_path,
-        :notice => "You have been deleted from our database"
-    else
-      @mentor.user.destroy
-      redirect_to admin_users_path,
-        :notice => "#{firstname} #{lastname} has been removed from the database."
-    end
-  end
+    @mentor.destroy
 
-  def rankings
-    @rankings = current_user.mentor.rankings.order(:rank)
-  end
-
-  private
-
-  def find_mentor
-    @mentor = Mentor.find(params[:id])
-  end
-
-  def authorize_admins_or_a_mentor!
-    find_mentor
-    unless current_user.admin? || user == @mentor
-      redirect_to root_path
-    end
-  end
-
-  def authorize_admins_or_students_or_a_mentor!
-    find_mentor
-    authenticate_user!
-    unless current_user.admin? || current_user.student.present? || current_user.mentor == @mentor
-      flash[:alert] = "You cannot access this page!"
-      redirect_to root_path
-    end
-  end
-
-  # update mentor
-  def update_mentor(mentor)
-    if mentor.update_attributes(params[:mentor])
-      set_update_message(mentor)
-      flash[:notice] = @@message
-      redirect_to mentor_path(@mentor)
-    else
-      flash[:notice] =  'There was a problem! ' +
-                      'Please make sure your first name, last name, job title, company, company type ' +
-                      '& email are all filled in below.'
-      render :action => "edit"
-    end
-  end
-
-  def set_update_message(mentor)
-    if user == mentor
-      @@message = "Your profile has been edited."
-    else
-      @@message = mentor.personal_first_name + ' ' + mentor.personal_last_name + "'s profile has been updated"
+    respond_to do |format|
+      format.html { redirect_to mentors_url }
+      format.json { head :no_content }
     end
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 	class StudentsController < ApplicationController
-  before_filter :authorizes_admins_or_mentors_or_a_student!,
+  before_filter :authorizes_admins_or_Students_or_a_student!,
   									only: 	[	:show]
 	before_filter :authorizes_admins_or_a_student!,
 										only: 	[	:edit,
@@ -11,143 +11,78 @@
 															:create,
 															:thanks]
 
-	def index
-		if current_user.student
-	    @user = user
-	    @cf_class = @user.cf_class
-	    @mentors = Mentor.joins(:user).where("approval = 1 AND cf_class = ?", @cf_class).order(:id)
-	    @mentor = @user.mentor
-      @rankings = current_user.student.studentrankings.order(:rank)
-	    @companies = []
-	    @mentors.each do |mentor|
-	    	company = mentor.career_company_private
-	    	unless @companies.include?(company)
-	    		@companies << mentor.career_company_private
-	    	end
-	    end
-			if params[:sort] == 'all'
-				@mentors = Mentor.joins(:user).where("approval = 1 AND cf_class = ?", @cf_class).order(:id)
-			elsif params[:sort]
-				@mentors = Mentor.joins(:user).where("approval = 1 AND career_company_private = ? AND cf_class = ?", params[:sort], @cf_class).order(:id)
-			end
-   	else
-    	flash[:alert] = "You don't have access to that page."
-   		redirect_to root_path
-   	end
-		respond_to do |format|
-			format.html
-			format.js
-		end
-	end
+def index
+    @students = Student.all
 
-	def show
-		respond_to do |format|
-			format.html
-			format.js
-		end
-	end
-
-	def edit
-		respond_to do |format|
-			format.html
-			format.js
-		end
-	end
-
-	def new
-		if current_user.role == "student" && !current_user.student
-			@student = Student.new
-		else
-			redirect_to students_path
-		end
-	end
-
-	def create
-		if current_user.role == "student" && !current_user.student
-			@student = Student.new(params[:student])
-			@student.user_id = current_user.id
-			if @student.save
-				redirect_to '/thanks'
-			else
-				flash[:alert] = 'Sorry, there was a problem. ' +
-												'Please make sure your first name, last name, & email are all filled in.'
-				render :action => "new"
-			end
-		else
-			redirect_to students_path
-		end
-	end
-
-	def thanks
-	end
-
-	def update
-		@student = Student.find(params[:id])
-
-		if current_user.student == @student || current_user.admin?
-			update_student(@student)
-		else
-			flash[:notice] = "You can only edit your own profile."
-			redirect_to students_path
-		end
-	end
-
-	def destroy
-		@student = Student.find(params[:id])
-		firstname = @student.personal_first_name
-		lastname = @student.personal_last_name
-		if current_user.student == @student
-			current_user.student.destroy
- 			flash[:notice] = "You have been deleted from our database."
-			redirect_to root_path
-		else
-			@student.user.destroy
-			flash[:notice] = "#{firstname} #{lastname} has been removed from the database."
-			redirect_to admin_users_path
-		end
-	end
-
-  def rankings
-    @rankings = current_user.student.studentrankings.order(:rank)
-  end
-
-	private
-
-	def find_student
-		@student = Student.find(params[:id])
-	end
-
-	def authorizes_admins_or_a_student!
-		find_student
-		unless current_user.admin? || user == @student
-			flash[:alert] = "You must be a student or admin to do that!"
-			redirect_to root_path
-		end
-	end
-
-  def authorizes_admins_or_mentors_or_a_student!
-  	find_student
-    authenticate_user!
-    unless current_user.admin? || current_user.mentor.present? || current_user.student == @student
-			flash[:alert] = "You must be a mentor or admin to do that!"
-      redirect_to root_path
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @students }
     end
   end
 
-  # update student's profile
-  def update_student(student)
-		if student.update_attributes(params[:student])
-			if user == student
-				message = "Your profile has been edited."
-			else
-				message = student.personal_first_name + ' ' + student.personal_last_name + ' has been edited.'
-			end
-			flash[:notice] = message
-			redirect_to student_path(student)
-		else
-			flash[:notice] = 	'There was a problem!' +
-												'Please make sure your first name, last name, & email are all filled in.'
-			render :action => "edit"
-		end
+
+  def show
+    @student = Student.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @student }
+    end
+  end
+
+
+  def new
+    @student = Student.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @student }
+    end
+  end
+
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+
+  def create
+    @student = Student.new(params[:student])
+
+    respond_to do |format|
+      if @student.save
+        format.html { redirect_to @student, notice: 'Student was successfully created.' }
+        format.json { render json: @student, status: :created, location: @student }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def update
+    @student = Student.find(params[:id])
+
+    respond_to do |format|
+      if @student.update_attributes(params[:student])
+        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+
+    respond_to do |format|
+      format.html { redirect_to students_url }
+      format.json { head :no_content }
+    end
   end
 end
