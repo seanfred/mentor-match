1655761405220e744febeeeb28206166f2892998
Michael Frankenfield <mfrank183@gmail.com>
2013-12-01 11:37:39 -0800

    deleted user controller, observer logic

--- app/observers/student_observer.rb
+++ app/observers/student_observer.rb
@@ -1,9 +1,9 @@
 class StudentObserver < ActiveRecord::Observer
 
-        #sends an automated notification to the student/mentor letting them know to watch their inbox
-        def after_create(student)
-                        Notifier.notification(student,
-                                "Mentor Match Confirmation",
-                                "Thanks for filling out your survey. You will be paired soon. Watch out for that email soon.").deliver
-        end
+        # #sends an automated notification to the student/mentor letting them know to watch their inbox
+        # def after_create(student)
+        #                 Notifier.notification(student,
+        #                         "Mentor Match Confirmation",
+        #                         "Thanks for filling out your survey. You will be paired soon. Watch out for that email soon.").deliver
+        # end
 end
